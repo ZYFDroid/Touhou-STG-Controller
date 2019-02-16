e513fcfba3b664fdd55125fdde605937bbbfd4c0
ZYFDroid <zyfdroid@qq.com>
2019-02-16 15:07:26 +0800

    Compact AndroidStudio

---
+++ Android_side/TouhouSTGController/app/build/generated/source/buildConfig/debug/com/zyfdroid/touhouremotectl/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.zyfdroid.touhouremotectl;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.zyfdroid.touhouremotectl";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.0.0.1";
+}
